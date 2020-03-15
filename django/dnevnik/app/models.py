0ace37e4f33dac579caf338a90df2ee6aa2ea38a
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2020-03-15 10:29:14 +0300

    models

--- django/dnevnik/app/models.py
+++ django/dnevnik/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Day(models.Model):
+    index = models.TextField(null=False, default="")
+
+    # done = models.TextField(null=False, default=0)
+
+    # num = models.TextField(null=False, default=0)
+
+    # to_closest = models.TextField(null=False, default=100)
+
+
+class Lesson(models.Model):
+    name = models.TextField(null=False, default="")
+
+    # deadline = models.DateTimeField(null=True)
+
+    # is_done = models.BooleanField(null=False, default=False)
+
+    # поле со временем, по-умолчанию равное текущему времени (на момент добавления)
+    # created_at = models.DateTimeField(auto_now_add=True)
+
+    project = models.ForeignKey(Day, on_delete=models.CASCADE, null=True)
+
+
