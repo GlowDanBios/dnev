40b2031cd60efd9a2c4863a5901e76be54b8ebb9
Ваше Имя <you@example.com>
2020-03-15 11:26:17 +0300

    out

--- django/dnevnik/app/views.py
+++ django/dnevnik/app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from .models import Day,Lesson
-
+from .models import Day, Lesson
 # Create your views here.
 
 
 def index(request):
-    days = Day.objects.all()
-    return render(request, 'index.html')
+    day = Day.objects.all()
+    lessons = []
+    for i in day:
+        lessons.append(Lesson.objects.filter(project_id = i.id))
+    return render(request, 'index.html', {"lessons":lessons, "day":day})
+
