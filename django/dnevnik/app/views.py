19bc08936927f29a3d81ea856c9eef0ea8298e80
Ваше Имя <you@example.com>
2020-03-15 15:33:40 +0300

    a

--- django/dnevnik/app/views.py
+++ django/dnevnik/app/views.py
@@ -17,121 +17,158 @@ def index(request):
 
 
 def add_lesson(request):
-    did = request.GET.get("did", None)
-    name = request.GET.get('name', None)
-    if did and name:
-        l = Lesson()
-        l.project = get_object_or_404(Day, pk=did)
-        l.name = name
-        l.save()
-    return redirect('/')
+    if request.user.is_authenticated:
+        did = request.GET.get("did", None)
+        name = request.GET.get('name', None)
+        if did and name:
+            l = Lesson()
+            l.project = get_object_or_404(Day, pk=did)
+            l.name = name
+            l.save()
+        return redirect('/')
+    else:
+        return redirect('/enter')
+
 
 
 def lesson(request):
-    lid = request.GET.get('lid', None)
-    if lid:
-        less = get_object_or_404(Lesson, pk=lid)
-        note = Note.objects.filter(lesson=less.id)
-        h = Htask.objects.filter(lesson=less.id)
-        return render(request, 'lesson.html', {'lesson': less, 'htask': h, 'note': note})
+    if request.user.is_authenticated:
+        lid = request.GET.get('lid', None)
+        if lid:
+            less = get_object_or_404(Lesson, pk=lid)
+            note = Note.objects.filter(lesson=less.id)
+            h = Htask.objects.filter(lesson=less.id)
+            return render(request, 'lesson.html', {'lesson': less, 'htask': h, 'note': note})
+        else:
+            return redirect('/')
     else:
-        return redirect('/')
+        return redirect('/enter')
 
 
 def add_htask(request):
-    note = request.GET.get("not", None)
-    i = request.GET.get("id", None)
-    if note:
-        n = Htask()
-        n.text = note
-        n.lesson = get_object_or_404(Lesson, pk=i)
-        n.save()
-    return redirect(f'/lesson?lid={i}')
+    if request.user.is_authenticated:
+        note = request.GET.get("not", None)
+        i = request.GET.get("id", None)
+        if note:
+            n = Htask()
+            n.text = note
+            n.lesson = get_object_or_404(Lesson, pk=i)
+            n.save()
+        return redirect(f'/lesson?lid={i}')
+    else:
+        return redirect('/enter')
 
 
 def add_note(request):
-    note = request.GET.get("not", None)
-    i = request.GET.get("id", None)
-    if note:
-        n = Note()
-        n.text = note
-        n.lesson = get_object_or_404(Lesson, pk=i)
-        n.save()
-    return redirect(f'/lesson?lid={i}')
+    if request.user.is_authenticated:
+        note = request.GET.get("not", None)
+        i = request.GET.get("id", None)
+        if note:
+            n = Note()
+            n.text = note
+            n.lesson = get_object_or_404(Lesson, pk=i)
+            n.save()
+        return redirect(f'/lesson?lid={i}')
+    else:
+        return redirect('/enter')
 
 
 def add_htask(request):
-    t = request.GET.get("not", None)
-    i = request.GET.get("id", None)
-    if t:
-        n = Htask()
-        n.text = t
-        n.lesson = get_object_or_404(Lesson, pk=i)
-        n.save()
-    return redirect(f'/lesson?lid={i}')
+    if request.user.is_authenticated:
+        t = request.GET.get("not", None)
+        i = request.GET.get("id", None)
+        if t:
+            n = Htask()
+            n.text = t
+            n.lesson = get_object_or_404(Lesson, pk=i)
+            n.save()
+        return redirect(f'/lesson?lid={i}')
+    else:
+        return redirect('/enter')
 
 
 def delete_lesson(request):
-    id = request.GET.get("iii", None)
-    if id:
-        o = get_object_or_404(Lesson, pk=id)
-        o.delete()
-    return redirect('/')
+    if request.user.is_authenticated:
+        id = request.GET.get("iii", None)
+        if id:
+            o = get_object_or_404(Lesson, pk=id)
+            o.delete()
+        return redirect('/')
+    else:
+        return redirect('/enter')
 
 def add_event(request):
-    event = request.GET.get("not", None)
-    i = request.GET.get("id", None)
-    if event:
-        e = Event()
-        e.text = event
-        e.day = get_object_or_404(Day, pk=i)
-        e.save()
-    return redirect(f'/day?did={i}')
+    if request.user.is_authenticated:
+        event = request.GET.get("not", None)
+        i = request.GET.get("id", None)
+        if event:
+            e = Event()
+            e.text = event
+            e.day = get_object_or_404(Day, pk=i)
+            e.save()
+        return redirect(f'/day?did={i}')
+    else:
+        return redirect('/enter')
 
 
 def day(request):
-    did = request.GET.get('did', None)
-    if did:
-        day = get_object_or_404(Day, pk=did)
-        event = Event.objects.filter(day=day.id)
-        return render(request, 'day.html', {'day':day, 'event': event})
+    if request.user.is_authenticated:
+        did = request.GET.get('did', None)
+        if did:
+            day = get_object_or_404(Day, pk=did)
+            event = Event.objects.filter(day=day.id)
+            return render(request, 'day.html', {'day':day, 'event': event})
+        else:
+            return redirect('/')
     else:
-        return redirect('/')
+        return redirect('/enter')
 
 
 def do(request):
-    id = request.GET.get('idd', None)
-    lid = request.GET.get('lid', None)
-    if id and lid:
-        ht = get_object_or_404(Htask, pk=id)
-        ht.is_done = True
-        ht.save()
-    return redirect(f'/lesson?lid={lid}')
+    if request.user.is_authenticated:
+        id = request.GET.get('idd', None)
+        lid = request.GET.get('lid', None)
+        if id and lid:
+            ht = get_object_or_404(Htask, pk=id)
+            ht.is_done = True
+            ht.save()
+        return redirect(f'/lesson?lid={lid}')
+    else:
+        return redirect('/enter')
 
 
 def clear_d_h(request):
-    h = Htask.objects.filter(is_done=True)
-    for i in h:
-        h.delete()
-    return redirect('/')
+    if request.user.is_authenticated:
+        h = Htask.objects.filter(is_done=True)
+        for i in h:
+            h.delete()
+        return redirect('/')
+    else:
+        return redirect('/enter')
 
 
 def delete_e(request):
-    id = request.GET.get("id", None)
-    did = request.GET.get("did", None)
-    if id:
-        e = get_object_or_404(Event, pk=id)
-        e.delete()
-    return redirect(f'/day?did={did}')
+    if request.user.is_authenticated:
+        id = request.GET.get("id", None)
+        did = request.GET.get("did", None)
+        if id:
+            e = get_object_or_404(Event, pk=id)
+            e.delete()
+        return redirect(f'/day?did={did}')
+    else:
+        return redirect('/enter')
 
 
 def delete_n(request):
-    id = request.GET.get("id", None)
-    lid = request.GET.get("lid", None)
-    if id:
-        n = get_object_or_404(Note, pk=id)
-        n.delete()
-    return redirect(f'/lesson?lid={lid}')
+    if request.user.is_authenticated:
+        id = request.GET.get("id", None)
+        lid = request.GET.get("lid", None)
+        if id:
+            n = get_object_or_404(Note, pk=id)
+            n.delete()
+        return redirect(f'/lesson?lid={lid}')
+    else:
+        return redirect('/enter')
 
 
 def reg(request):
@@ -154,8 +191,11 @@ def enter(request):
 
 
 def logut(request):
-    logout(request)
-    return redirect('/enter')
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('/enter')
+    else:
+        return redirect('/enter')
 
 
 def log(request):
